d8a96f1965a614e221a4426e79505902291789a0
ssedayzc <seda.yazici08@erzurum.edu.tr>
2023-03-09 21:57:46 +0300

    sartli bloklar

---
+++ Kodlamaio/SartliBloklar.py
@@ -0,0 +1,40 @@
+"""
+string değişkenler : yorumlar,ödev-ders vs konu başlıkları,hesap bilgileri...
+integer değişkenler : yorum sayısı,progress bar,kart bilgileri...
+
+"""
+
+# Mail alma onayı
+
+toApprove = input("Kodlama.io tarafından mail almak ister misiniz Y/N ?")
+
+if(toApprove == 'y' or toApprove == 'Y'):
+    print('Mail yoluyla bilgilendirilecekseniz.')
+
+else:
+    print('Tarafınıza mail iletilmeyecektir.')
+
+
+
+# Progress Bar
+progressBar = 0
+
+
+dersProgrami = input("Ders programını tamamladınız mı True/False ?")
+degerlendirme = input("Değerlendirmeyi tamamladınız mı True/False ?")
+odev1 = input("Ödev 1'i tamamladınız mı True/False ?")
+odev2 = input("Ödev 2'yi tamamladınız mı True/False ?")
+
+if(dersProgrami== "True" or dersProgrami== "true" ):
+    progressBar += 25
+
+if (degerlendirme == "True" or degerlendirme == "true"):
+    progressBar += 25
+
+if (odev1 == "True" or odev1 == "true"):
+    progressBar += 25
+
+if (odev2 == "True"or odev2 == "true"):
+    progressBar += 25
+
+print(f"{progressBar}% tamamlandı")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kodlama.io-Python-Selenium
+# Şartlı Bloklar
+
+Kodlama.io sayfasındaki şartlı blok kullanıldığını düşündüğüm bölümlerin kodlanması.
